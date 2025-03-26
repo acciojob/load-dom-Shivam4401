97e6b8e929e2182240ba4075237a27d783762602
Shivam kumar <146973981+Shivam4401@users.noreply.github.com>
2025-03-26 14:48:58 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
 //your JS code here. If required.
+document.addEventListener("DOMContentLoaded",()=>{
+	let message = document.createElement("p");
+	message.textContent = "DOM load success"
+	document.body.appendChild(message);
+})
